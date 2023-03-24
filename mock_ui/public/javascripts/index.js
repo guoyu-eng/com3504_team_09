45163aa448ac40199d2795d28f65088310b94c28
guoyu-eng <1147126254@qq.com>
2023-03-24 14:22:26 +0000

    ui

---
+++ mock_ui/public/javascripts/index.js
@@ -0,0 +1,16 @@
+
+//checks if variable isLoginCorrect is set. If not set Displays an alert
+
+function  checkForErrors(isLoginCorrect){
+
+    if  (!isLoginCorrect){
+
+    alert('login  or  password  is  incorrect');
+
+}
+
+}
+
+
+
+
