6ce15753a849fc8c0ee15f88ab14462a1aa7c3e3
Conor <60228993+CJWRWJC@users.noreply.github.com>
2023-05-19 08:10:40 +0100

    preparing to merge in changes from chat_fix_button branch

--- week1b/public/javascripts/index.js
+++ week1b/public/javascripts/index.js
@@ -1,22 +1,6 @@
+// script.js
+var birdButton = document.getElementById('birdButton');
 
-//checks if variable isLoginCorrect is set. If not set Displays an alert
-
-function  checkForErrors(isLoginCorrect){
-
-    if  (!isLoginCorrect){
-
-    alert('login  or  password  is  incorrect');
-
-}
-
-}
-
-
-//const aboutButton = document.getElementById('about-button');
-//aboutButton.addEventListener('click', () => {
-
-//    window.location.href = '/add_picture';
-//});
-
-
-
+birdButton.addEventListener('click', function() {
+    window.location.href = '/bird';
+});
--- week1b/routes/index.js
+++ week1b/routes/index.js
@@ -8,6 +8,7 @@ var showindex = require('../controller/showindex');
 const changeName = require('../controller/changeName');
 const fs = require('fs');
 const uploadDir = 'public/uploads/';
+const Bird = require('../model/bird');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -61,22 +62,22 @@ router.get('/bird', function(req, res, next) {
   res.render('bird', { title: 'birdData'});
 });
 
-//get the add picture  page
-router.get('/add_picture', function(req, res, next) {
-  res.render('add_picture', { title: 'add_picture'});
-});
+// //get the add picture  page
+// router.get('/add_picture', function(req, res, next) {
+//   res.render('add_picture', { title: 'add_picture'});
+// });
 
 // router.get('/details', function(req, res, next) {
 //   res.render('details', { title: 'details'});
 // });
-const Bird = require('../model/bird');
+
 
 
 router.get('/details', function(req, res) {
   const id = req.query.id;
   Bird.findOne({ _id: id }).exec()
       .then(bird => {
-        res.render('details', { title: 'add_picture', bird: bird });
+        res.render('details', { title: 'details', bird: bird });
       })
       .catch(err => {
         throw err;
@@ -97,7 +98,7 @@ router.get('/details', function(req, res) {
 //   readbird.getBirds(req,res);
 // });
 
-router.post('/bird', upload.single('inputImg'), function(req, res) {
+router.post('/', upload.single('inputImg'), function(req, res) {
   birdModel.create(req, res);
   // readbird.getBirds(req, res);
 
@@ -114,7 +115,7 @@ router.post('/bird', upload.single('inputImg'), function(req, res) {
 
 
 
-router.post('/add_picture', function(req, res, next) {
+router.get('/add_pictures', function(req, res, next) {
   res.render('bird', { title: " " });
 });
 
@@ -131,6 +132,9 @@ router.post('/change_name', changeName.changeName);
 router.get('/details/:id', function(req, res) {
   const id = req.params.id;
   console.log("")
+
+
+
 
   res.render('details', { id: id });
 });
