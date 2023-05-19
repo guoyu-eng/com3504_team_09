b9a6f6f793dc077b6f87a1f2e5562f898d33653c
Conor <60228993+CJWRWJC@users.noreply.github.com>
2023-05-19 07:46:19 +0100

    minor commit

---
+++ week1b/public/javascripts/changeName.js
@@ -0,0 +1,3 @@
+$('#changeNameForm').submit(function() {
+    alert('If successful, you will be redirected to the homepage. Otherwise, check your username is correct and that you are the owner of this entry.')
+})
